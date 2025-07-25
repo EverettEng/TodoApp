685fc0f6c94193f53780cb8e02ce16ddd1290219
Everett Eng <93930051+EverettEng@users.noreply.github.com>
2025-07-25 11:46:47 -0400

    trying to get the backend working part NINE

--- backend/database.py
+++ backend/database.py
@@ -3,13 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Use /data in Render, local file otherwise
-db_path = "/data/todo_app.db" if os.environ.get("RENDER") else "./todo_app.db"
+# DB in same directory as this file
+db_path = os.path.join(os.path.dirname(__file__), "todo_app.db")
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
-
-# Create empty DB file if it doesn't exist (optional)
-if not os.path.exists(db_path):
-    open(db_path, "a").close()
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
